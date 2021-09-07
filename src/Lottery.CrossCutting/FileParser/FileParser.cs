ccc9916eae1c4671554423926d5d0004dd6020ff
Paulo <pauloimmig@gmail.com>
2021-09-07 14:52:50 -0300

    refactor: Alter record usage

--- src/Lottery.CrossCutting/FileParser/FileParser.cs
+++ src/Lottery.CrossCutting/FileParser/FileParser.cs
@@ -24,46 +24,43 @@ namespace Lottery.CrossCutting.FileParser
             while (csv.Read())
             {
                 var record = new Participant
-                {
-                    Name = csv.GetField("Name"),
-                    FirstName = csv.GetField("First Name"),
-                    LastName = csv.GetField("Last Name"),
-                    Mail = csv.GetField("Email"),
-                    Status = csv.GetField("Status"),
-                    Action = csv.GetField("Action"),
-                    Entries = csv.GetField("Entries"),
-                    Details = csv.GetField("Details"),
-                    City = csv.GetField("City"),
-                    Region = csv.GetField("Region"),
-                    Country = csv.GetField("Country"),
-                    When = csv.GetField<DateTime>("When"),
-                    Facebook = csv.GetField("Facebook"),
-                    Instagram = csv.GetField("Instagram"),
-                    Twitter = csv.GetField("Twitter"),
-                    Youtube = csv.GetField("Youtube")
-                };
+                (
+                    Name: csv.GetField("Name"),
+                    FirstName: csv.GetField("First Name"),
+                    LastName: csv.GetField("Last Name"),
+                    Mail: csv.GetField("Email"),
+                    Status: csv.GetField("Status"),
+                    Action: csv.GetField("Action"),
+                    Entries: csv.GetField("Entries"),
+                    Details: csv.GetField("Details"),
+                    City: csv.GetField("City"),
+                    Region: csv.GetField("Region"),
+                    Country: csv.GetField("Country"),
+                    When: csv.GetField<DateTime>("When"),
+                    Facebook: csv.GetField("Facebook"),
+                    Instagram: csv.GetField("Instagram"),
+                    Twitter: csv.GetField("Twitter"),
+                    Youtube: csv.GetField("Youtube")
+                );
                 yield return record;
             }
         }
     }
 
-    public record Participant
-    {
-        public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Mail { get; set; }
-        public string Status { get; set; }
-        public string Action { get; set; }
-        public string Entries { get; set; }
-        public string Details { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string Country { get; set; }
-        public DateTime When { get; set; }
-        public string Facebook { get; set; }
-        public string Instagram { get; set; }
-        public string Twitter { get; set; }
-        public string Youtube { get; set; }
-    }
+    public record Participant(string Name,
+                              string FirstName,
+                              string LastName,
+                              string Mail,
+                              string Status,
+                              string Action,
+                              string Entries,
+                              string Details,
+                              string City,
+                              string Region,
+                              string Country,
+                              DateTime When,
+                              string Facebook,
+                              string Instagram,
+                              string Twitter,
+                              string Youtube);
 }
